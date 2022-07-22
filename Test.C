7c499b8a0149399b8356d3cc2dd1be2ceecb1bbb
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-22 13:53:34 +0100

    commented out code char harry

--- Test.C
+++ Test.C
@@ -2,8 +2,8 @@
 int main()
 {
    int a_7_harry_kicked_the_bucket; 
-   int _D_a_7_harry_kicked_the_bucket; 
-   char harry2 has; become good = 'x';
+   int _Da_7_harry_kicked_the_bucket; 
+   // char harry2_has_become_good; = 'x';
 
    printf ("I am beautiful"); 
 
