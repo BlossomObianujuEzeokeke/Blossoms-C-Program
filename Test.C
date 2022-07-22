99e6e34ba56c58dab12bc666a326da924322af57
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-22 01:30:34 +0100

    added character

--- Test.C
+++ Test.C
@@ -2,8 +2,8 @@
 int main()
 {
    int a_7_harry_kicked_the_bucket;
-   printf ("a 7harry? kicked_thebucket");
-   printf ("I am beautiful"); return 0;
+   printf ("I am beautiful"); 
+   return 0;
 }
 
 
