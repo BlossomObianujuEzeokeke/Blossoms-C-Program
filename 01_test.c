5457fbd38bb1f4a4fe64ac20441814a22b031995
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-25 00:09:05 +0100

    removed line hello harry

--- 01_test.c
+++ 01_test.c
@@ -3,7 +3,8 @@ int main()
 
 { 
    int b = 3;
-   int c = 200;
+   int d = 2.0123000012320001200;
+
    // int harry = 7;
    // char harry2 = 'c';
    // float harry3 = 5;
@@ -11,7 +12,7 @@ int main()
    
    printf("Hello Harry");
    printf("This is %d", b);
-   printf("This is integer %d", c);
+   printf("This is integer %d", d);
 
    // printf("This is % f");
    // printf("This is % c");
