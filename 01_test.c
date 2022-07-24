acd9f58370ca615ec3dea56fdedd9d39c3834570
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-25 00:29:24 +0100

    added int float char double values

--- 01_test.c
+++ 01_test.c
@@ -6,13 +6,16 @@ int main()
 // THIS IS INT SECTION //
    int a = 3;
    float b = 2.012;
-   char c = 'f';
+   char c = '$';
    double d = 325.05768482943892;
 
 // THIS IS THE LIBRARY\PRINT SECTION \\
    printf("Hello Harry");
-   printf("This is %d", b);
-   printf("This is a float %d", b);
+   printf("This is %d", a);
+   printf("This is a float %f", b);
+   printf("This is a character %c", c);
+   printf("This is a double %f", d);
+
 
   
    return 0;
