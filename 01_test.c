0c3cc8614ad09ff2ef3c3d18a814cda7bcaa0bda
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-24 23:21:44 +0100

    added print modulus d

---
+++ 01_test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{ 
+   int harry = 7;
+   char harry2 = 'c';
+   float harry3 = 5;
+
+   int Harry = 17;
+   printf("Hello Harry ");
+   printf("Hello Harry");
+   printf("This is % d", i)
+   
+   return 0;
+
+
+}
