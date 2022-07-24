416ef283ae00d1eb4871c65c647d13813658e725
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-24 23:52:04 +0100

    readded c =200

--- 01_test.c
+++ 01_test.c
@@ -2,15 +2,17 @@
 int main()
 
 { 
-   int b = 300;
-   int c = 500;
+   int b = 3;
+   int c = 200;
    // int harry = 7;
    // char harry2 = 'c';
    // float harry3 = 5;
 
    
    printf("Hello Harry");
-   printf("This is % d", b );
+   printf("This is %d", b);
+   printf("This is integer %d", c);
+
    // printf("This is % f");
    // printf("This is % c");
    
