60eea69a8f04fc507626afb5aa8ff066e950b411
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-24 02:26:35 +0100

    added comment

--- Test.C
+++ Test.C
@@ -4,6 +4,7 @@ int main()
 int a_7_harry_kicked_the_bucket; 
 
 // MY JOURNEY SO FAR //
+/* MY JOURNEY SO FAR */
 printf ("I am beautiful."); 
 printf ("I would love to see you and the world someday"); 
 
