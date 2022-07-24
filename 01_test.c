2ed3d127775be34db8eb39226cff06e0fc3d73a7
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-25 00:24:44 +0100

    removed two intstructions print f

--- 01_test.c
+++ 01_test.c
@@ -3,19 +3,18 @@
 int main()
 
 { 
+// THIS IS INT SECTION //
    int a = 3;
    float b = 2.012;
-   Doubles c = 325.903849400454;
+   char c = 'f';
+   double d = 325.05768482943892;
 
-
-   
+// THIS IS THE LIBRARY\PRINT SECTION \\
    printf("Hello Harry");
    printf("This is %d", b);
    printf("This is a float %d", b);
 
-   // printf("This is % f");
-   // printf("This is % c");
-   
+  
    return 0;
 
 
