6b011eaf7de4f30db788628406a8af1e8a0147d3
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-24 23:39:11 +0100

    added integer b=3

--- 01_test.c
+++ 01_test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main()
+
 { 
+   int b = 3;
    int harry = 7;
    char harry2 = 'c';
    float harry3 = 5;
@@ -8,9 +10,9 @@ int main()
    int Harry = 17;
    printf("Hello Harry ");
    printf("Hello Harry");
-   printf("This is % d");
-   printf("This is % f");
-   printf("This is % c");
+   printf("This is % d", b );
+   // printf("This is % f");
+   // printf("This is % c");
    
    return 0;
 
