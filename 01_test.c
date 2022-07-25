28bea0879038487ec2a8907d615bb57545538806
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-25 13:31:49 +0100

    removed auto keyword

--- 01_test.c
+++ 01_test.c
@@ -7,19 +7,19 @@ int main()
    int a = 3;
    float b = 2.12;
    char c = '$';
-   char c= '&'; 
-   double d = 325.057685;
+   char d= '&'; 
+   double e = 325.057685;
 
 // THIS IS THE LIBRARY\PRINT SECTION  //
    printf("Hello Harry\n");
    printf("This is %d\n", a);
    printf("This is a float %f\n", b);
    printf("This is a character %c\n", c);
+   printf("This is a character %c\n", d);
    printf("This is a double %f\n", d);
 
 
   
    return 0;
-
 
 }
