43f7baa5b73d2f41a3971d94fed0a1d0f591fe41
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-25 02:45:25 +0100

    added \n

--- 01_test.c
+++ 01_test.c
@@ -5,11 +5,12 @@ int main()
 { 
 // THIS IS INT SECTION //
    int a = 3;
-   float b = 2.012;
+   float b = 2.12;
    char c = '$';
-   double d = 325.05768482943892;
+   char c= '&'; 
+   double d = 325.057685;
 
-// THIS IS THE LIBRARY\PRINT SECTION \\
+// THIS IS THE LIBRARY\PRINT SECTION  //
    printf("Hello Harry\n");
    printf("This is %d\n", a);
    printf("This is a float %f\n", b);
