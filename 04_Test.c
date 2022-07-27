5dd5e31d5cada517c6123fb4f8cde8e6e8e77ffb
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-27 20:31:42 +0100

    added text on george

--- 04_Test.c
+++ 04_Test.c
@@ -5,7 +5,7 @@ int main(){
    printf("There once was a man named George\n");
    printf("he was 70 years old.\n");
    printf("He really liked the name George\n");
-   printf("but did not like\n being 70.\n");
+   printf("but did not like being 70.\n");
 
    return 0;
 }
