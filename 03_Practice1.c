150320c55381d0ff766e507190e1b60718117bef
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-27 02:02:06 +0100

    added area of rectangle 23

--- 03_Practice1.c
+++ 03_Practice1.c
@@ -1,9 +1,21 @@
 #include<stdio.h>
 
 int main(){ 
-   int a = 4;
-   float b = 7.5;
-   printf("The area of a rectangle is %d, %f", a*b);
+   // int a = 4;
+   // float b = 7.5;
+   // printf("The area of a rectangle is %d,", a*b);
+int a;
+int b;
+printf("Enter the value of length\n");
+scanf("%d", &a);
+
+printf("Enter the value of breadth\n");
+scanf("%d", &b);
+
+
+
+
+
    return 0;
 
 }
