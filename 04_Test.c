3c4d0fb9884e7d01c54305d0acd60ffa3eee3751
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-27 20:22:34 +0100

    added backlashes

--- 04_Test.c
+++ 04_Test.c
@@ -2,10 +2,11 @@
 
 
 int main(){
-      printf("/\n");
-      printf("/  \n");
-     printf("/\n");
-      printf("/\n");
+      printf("     /|\n");
+      printf("    / |\n");
+      printf("   /  |\n");
+      printf("  /___|\n");
+
    return 0;
 }
 
