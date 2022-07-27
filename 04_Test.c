357ac84588096f7c9f029527ddc7d444c5f1f520
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-27 20:45:09 +0100

    added character name John

--- 04_Test.c
+++ 04_Test.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 
 
-int main(){
+int main()
+{
+
+   char characterName[] = "John";
    printf("There once was a man named George\n");
    printf("he was 70 years old.\n");
    printf("He really liked the name George\n");
