7c8da6d397845bf53b1070d5e60597ff81c2ce1d
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-27 20:54:03 +0100

    added int character age

--- 04_Test.c
+++ 04_Test.c
@@ -5,6 +5,7 @@ int main()
 {
 
    char characterName[] = "John";
+   int characterAge = ""
    printf("There once was a man named George\n");
    printf("he was 70 years old.\n");
    printf("He really liked the name George\n");
