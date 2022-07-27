a57962a9ba96f385c8f432a69aeaa8b812217f51
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-27 02:54:22 +0100

    added area of circle

--- 03_Practice1.c
+++ 03_Practice1.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 
 int main(){ 
+/*  HARDCODE, EMBEDDED */
    // int a = 4;
-   // float b = 7.5;
+   // float b = 7;
    // printf("The area of a rectangle is %d,", a*b);
-int a;
-int b;
-printf("Enter the value of length\n");
-scanf("%d", &a);
 
-printf("Enter the value of breadth\n");
-scanf("%d", &b);
-printf("The area of a rectangle is %d", a*b);
+/* FOR USER INPUT */
+// int a;
+// int b;
+// printf("Enter the value of length\n");
+// scanf("%d", &a);
+
+// printf("Enter the value of breadth\n");
+// scanf("%d", &b);
+// printf("The area of a rectangle is %d", a*b);
+
+int r = 4;
+printf("The area of a circle is %f", 3.14*r*r);
 
 
 
