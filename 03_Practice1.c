63a40e40d5bfa743ced77b544f5074047b0cec92
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-29 00:58:24 +0100

    creating square image

--- 03_Practice1.c
+++ 03_Practice1.c
@@ -20,17 +20,18 @@ int main(){
 // int r = 4;
 // printf("The area of a circle is %f\n", 3.14*r*r);
 
-// int church = 1000;
-// printf("The number of church members is %d", church);
+int church = 1000;
+int school = 2000
+printf("The number of church members is %d and %d", church, school);
 
 // int h = 5;
 // printf("The volume of a cylinder is %f", 3.14*r*r*h);
 
 printf(" |----------------------\n")
+printf(" |                      |\n")
 printf(" |----------------------\n")
 printf(" |----------------------\n")
-printf(" ----------------------\n")
-printf(" ----------------------\n")
+printf(" |----------------------\n")
 
 
 
