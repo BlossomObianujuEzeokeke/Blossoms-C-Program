6b0d03675fcfee2a45772d16717bc99c3218a2be
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-28 08:30:14 +0100

    added variable num 6

--- Test.C
+++ Test.C
@@ -1,15 +1,22 @@
 #include <stdio.h>
 int main()
-{ 
+
+{
+
 // INTEGERS, FLOATS, DOUBLES, CHAR
+// ----------------ADDING A VARIABLE----------------------------------------->
 // int favNum = 90;
 // char favChar = 'i';
+// ------------------------------------------------------------------------>
 // printf("My %s number is %f and, I also like the %c\n", "favorite", 500.567, '$');
 // printf("My %s number is %d %s, I also like the %c", "favorite", 500, "and", favChar);
 
 /*   WORKING WITH NUMBERS */
-printf("My favorite number is %f\n", 8.9);
-printf("%d", 5 / 4.0);
+// printf("My favorite number is %f\n", 8.9);
+// printf("%f", 5 / 4.0);
+// -----------------ADDING A VARIABLE-------------------------------------->
+INT NUM = 6;
+PRINTF("%D", NUM);
 
 
 
