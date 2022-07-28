a47d05995d5636c7be7cc996c77deb7f6280217c
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-28 06:22:55 +0100

    added favNum 90

--- Test.C
+++ Test.C
@@ -1,12 +1,10 @@
 #include <stdio.h>
 int main()
-
 { 
 
-int favNum = 90
-printf("My %s number is %f and, \nI also like the %c", "favorite", 500.567, '$');
-printf("My %s number is %d and, \nI also like the %c", "favorite", favNum, '$');
-
+int favNum = 90;
+printf("My %s number is %f and, I also like the %c\n", "favorite", 500.567, '$');
+printf("My %s number is %d and, I also like the %c", "favorite", favNum, '$');
 
 return 0;
 }
