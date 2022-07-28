7ea88f677b6094e2da80e6e3a2997e7ef1997722
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-28 06:04:35 +0100

    chnged value to 500.567

--- Test.C
+++ Test.C
@@ -3,6 +3,7 @@ int main()
 
 { 
 printf("My %s number is %f and, \nI also like the %c", "favorite", 500.567, '$');
+printf("My %s number is %f and, \nI also like the %c", "favorite", 500.567, '$');
 
 
 return 0;
