a35d64ab7952a7f0d81772738e8b860dcb622671
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-28 07:27:14 +0100

    added float decimal 8.9

--- Test.C
+++ Test.C
@@ -1,11 +1,16 @@
 #include <stdio.h>
 int main()
 { 
+// INTEGERS, FLOATS, DOUBLES, CHAR
+// int favNum = 90;
+// char favChar = 'i';
+// printf("My %s number is %f and, I also like the %c\n", "favorite", 500.567, '$');
+// printf("My %s number is %d %s, I also like the %c", "favorite", 500, "and", favChar);
 
-int favNum = 90;
-char favChar = 'i';
-printf("My %s number is %f and, I also like the %c\n", "favorite", 500.567, '$');
-printf("My %s number is %d %s, I also like the %c", "favorite", 500, "and", favChar);
+/*   WORKING WITH NUMBERS */
+printf("%f", 8.9)
+
+
 
 return 0;
 }
