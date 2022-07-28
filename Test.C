28e8a0335403fa3627e76a4c428cfe28f02337fa
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-28 04:39:16 +0100

    added hello world

--- Test.C
+++ Test.C
@@ -3,9 +3,9 @@ int main()
 {
 int age = 40;
 double gpa = 3.6;
-char grade = 'A'
-char phrase[] = "Giraffe Academy"
-printf("Hello \n world")
+char grade = 'A';
+char phrase[] = "Giraffe Academy";
+printf("Hello \"world");
 
 
 return 0;
