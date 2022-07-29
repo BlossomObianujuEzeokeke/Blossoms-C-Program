4b3024ee5868df46a2b55889326318b035d8c448
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-29 01:05:10 +0100

    working on functions

--- Test.C
+++ Test.C
@@ -11,12 +11,19 @@ int main()
 // printf("My %s number is %f and, I also like the %c\n", "favorite", 500.567, '$');
 // printf("My %s number is %d %s, I also like the %c", "favorite", 500, "and", favChar);
 
-/*   WORKING WITH NUMBERS */
+//------------------WORKING WITH NUMBERS----------------------------------->
 // printf("My favorite number is %f\n", 8.9);
 // printf("%f", 5 / 4.0);
 // -----------------ADDING A VARIABLE-------------------------------------->
-int num = 6;
-printf("%d", num);
+// int num = 6;
+// printf("%d", num);
+
+// ------------------FUNCTIONS--------------------------------------------->
+printf("%f", pow(2, 3) );
+
+
+
+
 
 
 
