ec2e1e52b495135b0d0d8a3656c337fe5975f6fe
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-31 23:04:15 +0100

    working on functions

--- 04_Test.c
+++ 04_Test.c
@@ -9,10 +9,28 @@ int main()
    printf("There once was a man named %s\n", characterName);
    printf("he was %d years old.\n", characterAge);
 
-   characterAge = 30;
-   printf("He really liked the name %s\n", characterName);
-   printf("but did not like being %d\n", characterAge);
-   printf("he did like being %d\n", characterAge);
+   // characterAge = 30;
+   // printf("He really liked the name %s\n", characterName);
+   // printf("but did not like being %d\n", characterAge);
+   // printf("he did like being %d\n", characterAge);
+
+char character name[] = "Samson";
+int favNumber = 50;
+printf("A man named %s just turned %d", character name, favNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
 
    return 0;
 }
