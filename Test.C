54dfad63c0feabcc5f1ccd11e7c7983abee8313e
BlossomEze <ezeokekeobianuju@gmail.com>
2022-07-31 23:37:06 +0100

    added function

--- Test.C
+++ Test.C
@@ -55,7 +55,8 @@ int x = 15;
 int y = 10;
 int sum = x + y;
 int sub = x - y;
-printf("Add these two %i: subtract these two %d", pow(4,3), sum, sub);
+printf("Add these two %i: subtract these two %d\n", sum, sub,);
+printf("Get the power of this" pow(4,3));
 
 
 
