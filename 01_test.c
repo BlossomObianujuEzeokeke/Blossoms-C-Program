dd7af0b3d5094822e5cc70bf363b4dc4e26c9459
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-06 03:05:22 +0100

    Building a basic calculator

--- 01_test.c
+++ 01_test.c
@@ -19,15 +19,19 @@ int main()
    // printf("This is a string %s",)
 
 //--------------BUILDING A BASIC CALCULATOR--------------//
-int num1;
-int num2;
-printf("Enter first number: ");
-scanf("%d", &num1);
-printf("Enter second number: ");
-scanf("%d", &num2);
+// int num1;
+// int num2;
+// printf("Enter first number: ");
+// scanf("%d", &num1);
+// printf("Enter second number: ");
+// scanf("%d", &num2);
 
-printf("Answer: %d", num1 + num2);
+// printf("Answer: %d", num1 + num2);
 
+double num1;
+double num2;
+printf("Enter your first number:");
+scanf("%lf", &num1)
 
 
 
