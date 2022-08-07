d5dabc1eab5de3570963674be4960cb77bda4f13
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-08 00:15:49 +0100

    added arg to fgets

--- 02_Test.c
+++ 02_Test.c
@@ -8,11 +8,11 @@ char Monday[50];
 char Tuesday[20];
 char Wednesday[20];
 
-printf("Enter days of the week":);
+printf("Enter days of the week:");
 fgets( Monday, 50, stdin);
 fgets(Tuesday, 20, stdin);
 fgets(Wednesday, 20, stdin);
-printf("Your days of the week are \"%s\", \"%s\", \"%s\" ", Monday, Tuesday, Wednesday);
+printf("Your days of the week are \"%s\"\n, \"%s\"\n, \"%s\"\n ", Monday, Tuesday, Wednesday);
 
 
 
