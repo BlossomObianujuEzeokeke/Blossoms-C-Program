8aa3999f6225547aa5c504e7877b3fbbf711cf3b
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-08 23:59:06 +0100

    Arrays

--- 01_test.c
+++ 01_test.c
@@ -65,25 +65,25 @@ int main()
 
 
 //-------------------------------------------------------------//
-char color[20];
-char pluralNoun[20];
-char celebrityL[20];
-char celebrityF[20];
+// char color[20];
+// char pluralNoun[20];
+// char celebrityL[20];
+// char celebrityF[20];
 
-printf("Enter a color:");
-scanf("%s", &color);
+// printf("Enter a color:");
+// scanf("%s", &color);
 
-printf("Enter a Plural noun:");
-scanf("%s", pluralNoun);
+// printf("Enter a Plural noun:");
+// scanf("%s", pluralNoun);
 
-printf("Enter a celebrity Name:");
-scanf("%s %s", celebrityL, celebrityF );
+// printf("Enter a celebrity Name:");
+// scanf("%s %s", celebrityL, celebrityF );
  
-printf("Roses are %s\n", color );
-printf("%s are blue\n", pluralNoun );
-printf("I love %s %s", celebrityL, celebrityF);
+// printf("Roses are %s\n", color );
+// printf("%s are blue\n", pluralNoun );
+// printf("I love %s\n %s", celebrityL, celebrityF);
 
-
+//--------------- ARRAYS --------------------//
 
 
 
