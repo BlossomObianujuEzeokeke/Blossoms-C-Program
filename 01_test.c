97172d41f0a25ef0ab63cebaf678d9fa8a39d703
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-08 22:21:21 +0100

    added variable

--- 01_test.c
+++ 01_test.c
@@ -65,22 +65,23 @@ int main()
 
 
 //-------------------------------------------------------------//
-// char color[20];
-// char pluralNoun[20];
-// char celebrity[20];
+char color[20];
+char pluralNoun[20];
+char celebrityL[20];
+char celebrityF[20];
 
-// printf("Enter a color:");
-// scanf("%s", &color);
+printf("Enter a color:");
+scanf("%s", &color);
 
-// printf("Enter a Plural noun:");
-// scanf("%s", pluralNoun);
+printf("Enter a Plural noun:");
+scanf("%s", pluralNoun);
 
-// printf("Enter a celebrity Name:");
-// scanf("%s", celebrity);
+printf("Enter a celebrity Name:");
+scanf("%s %s", celebrityL, celebrityF );
  
-// printf("Roses are %s\n", color );
-// printf("%s are blue\n", pluralNoun );
-// printf("I love %s", celebrity);
+printf("Roses are %s\n", color );
+printf("%s are blue\n", pluralNoun );
+printf("I love %s %s", celebrityL, celebrityF);
 
 
 
