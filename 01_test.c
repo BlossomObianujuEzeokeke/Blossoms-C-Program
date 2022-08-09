423497aea65457969d54becf2fc60da0eaa34f42
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-09 22:49:37 +0100

    working on Arrays

--- 01_test.c
+++ 01_test.c
@@ -92,7 +92,7 @@ printf("%d\n", house[0] );
 printf("%d\n", house[1] );
 printf("%d\n", house[2] );
 
-
+//----------- CHANGING AN ARRAY ELEMENT ---------//
 // int luckyNumbers[] = {4, 8, 15, 16, 23, 42};
 // luckyNumbers[0] = 15;
 // luckyNumbers[4] = 25;
