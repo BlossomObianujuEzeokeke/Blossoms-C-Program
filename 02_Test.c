81a4782fedeb574300dc8db06ccf7454f8274ebf
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-13 23:40:41 +0100

    WORKING ON ELSE IF AND COMPARISON OPERATORS WHILST BUILDING MAX FUNCTION

--- 02_Test.c
+++ 02_Test.c
@@ -231,8 +231,8 @@
 int max(int num1, int num2, int num3)
 {
   int result;
- if(num1 >= num2 && num1 >= num3){result = num1;}
- else if(num2 >= num1 && num2 >= num3){result = num2;}
+ if(num1 <= num2 && num1 <= num3){result = num1;}
+ else if(num2 <= num1 && num2 <= num3){result = num2;}
  else{result = num3;}
 }
 
@@ -240,8 +240,7 @@ int main()
 {
   printf("%i", max(1, 2, 3) );
   return 0;
-}
-
+}<
 
 
 
