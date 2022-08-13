2943efe400bb1e4d876e48fc2b1a5c220811bd06
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-14 00:13:39 +0100

    WORKING ON ELSE IF AND COMPARISON OPERATORS WHILST BUILDING MAX FUNCTION

--- 02_Test.c
+++ 02_Test.c
@@ -225,13 +225,13 @@
 //   return 0;
 // }
 
-
+//----------EXAMPLE 2 NOT EQUAL TO-------//
 #include <stdio.h>
 int max(int num1, int num2, int num3)
 {
   int result;
- if(num1 != num2 && num1 != num3){result = num1;}
- else if(num2 != num1 && num2 != num3){result = num2;}
+ if(num1 == num2 && num1 == num3){result = num1;}
+ else if(num2 == num1 && num2 == num3){result = num2;}
  else{result = num3;}
 }
 
