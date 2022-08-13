ce7c667c8456c01c6e12d24ef0b2003368724a69
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-13 23:32:33 +0100

    Working on if statements

--- 02_Test.c
+++ 02_Test.c
@@ -209,17 +209,19 @@
 //   return 0;
 // }
 
-//-------------EXAMPLE 2-------------//
+//----------------------ELSE IF-------------------//
 #include <stdio.h>
 int max(int num1, int num2, int num3)
 {
   int result;
- 
+ if(num1 >= num2 && num1 >= num3){result = num1;}
+ else if(num2 >= num1 && num2 >= num3){result = num2;}
+ else{result = num3;}
 }
 
 int main()
 {
-  printf("%i", max(40, 10) );
+  printf("%i", max(1, 2, 3) );
   return 0;
 }
 
