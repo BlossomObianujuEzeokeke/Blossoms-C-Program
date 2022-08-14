e9dcac3cb893297f0e98938f27d1c1f281401a5b
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-14 23:41:00 +0100

    BUILDING A BETTER CALCULATOR

--- 03_Practice1.c
+++ 03_Practice1.c
@@ -33,6 +33,16 @@
 // printf(" |      |    |    |    |    |\n");
 // printf(" |-------|-----|---|----|---|\n");
 
+//--------------BUILDING A BASIC CALCULATOR--------------//
+// int num1;
+// int num2;
+// printf("Enter first number: ");
+// scanf("%d", &num1);
+// printf("Enter second number: ");
+// scanf("%d", &num2);
+
+// printf("Answer: %d", num1 + num2);
+
 //---------------- BUILDING A BETTER CALCULATOR -------------//
 #include <stdio.h>
 int main()
