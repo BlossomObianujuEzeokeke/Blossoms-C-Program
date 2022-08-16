81ab99f9ba92a3fb0bc6f2cdc425f66d881fb2c3
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-16 22:50:37 +0100

    BUILDING A BETTER CALCULATOR

--- 03_Practice1.c
+++ 03_Practice1.c
@@ -80,14 +80,45 @@ else if(op == '-'){printf("%lf", num1 - num2);}
 else if(op == '/'){printf("%lf", num1 / num2);}
 else if(op == '*'){printf("%lf", num1 * num2);}
 else{printf("Invalid Operator");}
-
  
+   return 0;
+}
+//-----------------------------------------------//
+#include <stdio.h>
+int main()
+{
+int num1;
+int num2;
+char op;
+
+printf("Enter a number:");
+scanf("%d", &num1);
+printf(" %c", &op);
+printf("Enter a number:");
+scanf("%d", &num2);
+
+if(){}
+
+
+
+
+
+
+
+
+
    return 0;
 }
 
 
 
 
-//    return 0;
 
-// }
+
+
+
+
+
+//-------------- SWITCH STATEMENTS ----------------//
+
+
