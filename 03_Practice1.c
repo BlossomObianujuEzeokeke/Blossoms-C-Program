91dd633adc0ffeddd4fdddceeb79ff4f628646f1
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-16 12:02:51 +0100

    Building a Calculator

--- 03_Practice1.c
+++ 03_Practice1.c
@@ -43,6 +43,40 @@
 
 // printf("Answer: %d", num1 + num2);
 
+
+#include <stdio.h>
+int main()
+{
+   int num1;
+   int num2;
+   printf("Enter your 1st number:");
+   scanf("%d", &num1);
+    printf("Enter your second number:");
+   scanf("%d", &num2);
+
+   printf("Answer: %d", num1 + num2);
+   return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //---------------- BUILDING A BETTER CALCULATOR -------------//
 // #include <stdio.h>
 // int main()
