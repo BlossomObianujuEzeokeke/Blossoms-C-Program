d441dcabc3164d4454bd2ded134e4c4c7f041a43
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-16 23:01:19 +0100

    BUILDING A BETTER CALCULATOR

--- 03_Practice1.c
+++ 03_Practice1.c
@@ -95,10 +95,10 @@ char op;
 printf("Enter a number:");
 scanf("%d", &num1);
 
-printf("Enter an operator");
+printf("Enter an operator: ");
 scanf(" %c", &op);
 
-printf("Enter a number:");
+printf("Enter a number: ");
 scanf("%d", &num2);
 
 if(op == '+'){printf("%d", num1 + num2);}
