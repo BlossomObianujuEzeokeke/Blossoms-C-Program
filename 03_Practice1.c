2347b9376e5f9f3513976d05fcb49e0e4c7d2fbb
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-16 21:06:36 +0100

    Building a basic calculator

--- 03_Practice1.c
+++ 03_Practice1.c
@@ -43,39 +43,21 @@
 
 // printf("Answer: %d", num1 + num2);
 
+//------------------------------------//
+// #include <stdio.h>
+// int main()
+// {
+//    int num1;
+//    int num2;
+//    printf("Enter your 1st number:");
+//    scanf("%d", &num1);
+//     printf("Enter your second number:");
+//    scanf("%d", &num2);
 
-#include <stdio.h>
-int main()
-{
-   int num1;
-   int num2;
-   printf("Enter your 1st number:");
-   scanf("%d", &num1);
-    printf("Enter your second number:");
-   scanf("%d", &num2);
+//    printf("Answer: %d", num1 == num2);
+//    return 0;
 
-   printf("Answer: %d", num1 == num2);
-   return 0;
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// }
 
 //---------------- BUILDING A BETTER CALCULATOR -------------//
 // #include <stdio.h>
@@ -93,7 +75,6 @@ int main()
 // printf(" %c", &op);
 
 // if (op == '+'){printf("%f", num1 + num2);}
-// else if (op == '-'){printf("%f", num1 - num2);}                                                                                                                                                                                                     
 
 
 
