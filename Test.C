11c22c3a4d2cf4bf1d66e2f524a44765445bf8f2
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-23 06:41:03 +0100

    if statements

--- Test.C
+++ Test.C
@@ -193,27 +193,40 @@
 // }
 
 //---------------- FIND THE HYPOTENUSE OF A CIRCLE -------------//
+// #include <stdio.h>
+// #include <math.h>
+
+// int main(){
+// double A;
+// double B;
+// double C;
+
+// printf("Enter side A: ");
+// scanf("%lf", &A);
+
+// printf("Enter side B: ");
+// scanf("%lf", &B);
+
+// C = sqrt(A*A + B*B);
+// printf("Side C is %lf", C);
+
+//    return 0;
+// }
+
+//------------ IF STATEMENTS ----------//
 #include <stdio.h>
-#include <math.h>
-
 int main(){
-double A;
-double B;
-double C;
+   int age;
 
-printf("Enter side A: ");
-scanf("%lf", &A);
+printf("Enter your age: ");
+scanf("%d", &age);
 
-printf("Enter side B: ");
-scanf("%lf", &B);
 
-C = sqrt(A*A + B*B);
-printf("Side C is %lf", C);
+
+
 
    return 0;
 }
-
-
 
 
 
