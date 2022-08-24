0b8b576537edc8350a30400620efb9f3d5a7f198
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-25 00:56:41 +0100

    celsius

--- 01_test.c
+++ 01_test.c
@@ -171,8 +171,62 @@
 //    return 0;
 // }
 
+//-------------------------------------------
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+char unit;
+float temp;
+
+printf("\nIs the temperature in (F) or (c)? ");
+scanf("%c", &unit);
+
+unit = toupper(unit);
+
+switch(unit){
+case 'C':
+printf("\nEnter the temp in Celsius: ");
+scanf("%f", &temp);
+temp = (temp * 9 / 5) + 32;
+printf("\nThe temp in Fahrenheit is %.1f", temp);
+break;
 
 
+case 'F':
+printf("\nEnter the temp in Fahrenheit: ");
+scanf("%f", &temp);
+temp = (temp * 9 / 5) + 32;
+printf("\nThe temp in Fahrenheit is %.1f", temp);
+break;
+
+return 0;
+}
+
+
+
+// unit = toupper(unit);
+
+// if(unit == 'C'){
+// printf("\nEnter the temp in Celsius: ");
+// scanf("%f", &temp);
+// temp = (temp * 9 / 5) + 32;
+// printf("\nThe temp in Fahrenheit is %.1f", temp);
+// }
+
+// else if(unit == 'F'){
+// printf("\nEnter the temp in Fahrenheit: ");
+// scanf("%f", &temp);
+// temp = ((temp - 32) * 5) / 9;
+// printf("\nThe temp in Fahrenheit is: %.1f", temp);
+// }
+
+// else{
+//    printf("%c is not a valid unit of measurement", unit);
+// }
+
+//    return 0;
+// }
 
 
 
