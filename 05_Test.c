8cb430816224779c2d09678e5eb8a1cd429fdda7
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-31 06:21:20 +0100

    Calling a function in main

--- 05_Test.c
+++ 05_Test.c
@@ -1,17 +1,29 @@
 //-------- CALLING A FUNCTION IN MAIN -----------//
+// #include <stdio.h>
+
+// void myBirthday()
+// {
+// printf("\nHello my birthday is today!");
+// printf("\nHello my birthday is today!");
+// printf("\nHello my birthday is today!");
+// }
+
+// int main(){
+// myBirthday();
+// myBirthday();
+// myBirthday();
+//    return 0;
+// }
+
 #include <stdio.h>
 
-void myBirthday()
+void myHome(char Blossom[],int age, double gpa)
 {
-printf("\nHello my birthday is today!");
-printf("\nHello my birthday is today!");
-printf("\nHello my birthday is today!");
+   printf("\n%s lives in London, she is %d years old and, her gpa is %lf", Blossom, age, gpa);
 }
 
-
 int main(){
-myBirthday();
-myBirthday();
-myBirthday();
-   return 0;
+   myHome("Blossom");
+   myHome("Blossom");
+   myHome("Blossom");
 }
