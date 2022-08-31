84ac01fbaf69aa18a6a7280b6e874dd7905ea3c8
BlossomEze <ezeokekeobianuju@gmail.com>
2022-08-31 06:02:27 +0100

    Working on functions

--- 05_Test.c
+++ 05_Test.c
@@ -1,6 +1,17 @@
+//-------- CALLING A FUNCTION IN MAIN -----------//
 #include <stdio.h>
+#include <math.h>
+
+void myBirthday(){
+printf("\nHello my birthday is today!");
+printf("\nHello my birthday is today!");
+printf("\nHello my birthday is today!");
+}
+
 
 int main(){
-
+myBirthday();
+myBirthday();
+myBirthday();
    return 0;
 }
