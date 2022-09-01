ead2c7fe71aea927c11ab4d76f22305f13029b27
BlossomEze <ezeokekeobianuju@gmail.com>
2022-09-01 09:59:43 +0100

    For loops

--- 05_Test.c
+++ 05_Test.c
@@ -72,7 +72,7 @@ int main(){
 // for(int i = 1; i <= 10; i++ )
 // for(int i = 1; i <= 10; i+= 2 )
 // for(int i = 10; i >= 1; i-- )
-for(int i = 10; i >= 1; i-= 2)
+for(int i = 10; i >= 1; i--)
 {
 printf("%d\n", i);
 
