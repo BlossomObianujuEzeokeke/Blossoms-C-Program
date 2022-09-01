516b5b63b66b9ab827af9c01981068c513a19903
BlossomEze <ezeokekeobianuju@gmail.com>
2022-09-01 10:19:46 +0100

    For loops

--- 05_Test.c
+++ 05_Test.c
@@ -66,21 +66,20 @@
 // }
 
 //-------- FOR LOOPS(DECREMENT) ----------//
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(){
+// int main(){
 // for(int i = 1; i <= 10; i++ )
 // for(int i = 1; i <= 10; i+= 2 )
 // for(int i = 10; i >= 1; i-- )
-for(int i = 10; i >= 1; i--)
-{
-printf("%d\n", i);
+// for(int i = 10; i >= 1; i--)
+// {
+// printf("%d\n", i);
+// }
+   // return 0;
+// }
 
-}
-   return 0;
-}
-
-
+//---------- 
 
 
 
