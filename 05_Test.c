837c82c5d5a86c2b5d6e6acedbd6aeca680fc9f6
BlossomEze <ezeokekeobianuju@gmail.com>
2022-09-08 06:47:19 +0100

    2D Arrays

--- 05_Test.c
+++ 05_Test.c
@@ -299,14 +299,20 @@
 // }
 
 //-------- SECOND EXAMPLE -----//
-#include<stdio.h>
-int main(){
-  
-char cars[1][3] = {
-                 {"Mustang", "Corvette", "Toyota"}
-                  };
+// #include<stdio.h>
+// #include <string.h>
 
-printf("%c", cars[0]);
+// int main(){
+  
+// char cars[][10] = {"Mustang", "Corvette", "Toyota"};
+
+// cars[0] = "Tesla";
+// strcpy(cars[0], "Tesla");
+// for(int i = 0; i < sizeof(cars)/sizeof(cars[0]); i++)
+// {
+//   printf("%s\n", cars);
+
+// }
   
   return 0;
 }
